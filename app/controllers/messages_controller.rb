19e0b9f0dac29b1495e328da5efc8a5311571d44
Eko Teguh Widodo <id.ekoteguhw@gmail.com>
2018-03-11 11:03:50 +0700

    Implement edit and delete

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -17,6 +17,29 @@ class MessagesController < ApplicationController
 		end
 	end
 
+	def show
+		@message = Message.find(params[:id])
+	end
+
+	def edit
+		@message = Message.find(params[:id])
+	end
+
+	def update
+		@message = Message.find(params[:id])
+		if @message.update(message_params)
+			redirect_to '/messages'
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@message = Message.find(params[:id])
+		@message.destroy
+		redirect_to '/messages'
+	end
+
 	private
 
 	def message_params
